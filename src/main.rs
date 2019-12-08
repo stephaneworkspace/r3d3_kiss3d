34ad3ba1c089a507f8bb409d752c63e5d7c318b6
stephaneworkspace <s.bressani@bluewin.ch>
2019-12-08 18:03:10 +0100

    Include astro lib

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
+extern crate astro;
 extern crate kiss3d;
 extern crate nalgebra as na;
 
+use astro::*;
 use kiss3d::window::Window;
 use na::{Translation2, UnitComplex};
 
 fn main() {
+    // Init Date 01-01-2020
+    let day_of_month = time::DayOfMonth {
+        day: 1,
+        hr: 0,
+        min: 0,
+        sec: 0.0,
+        time_zone: 0.0,
+    };
+    let date = time::Date {
+        year: 2020,
+        month: 1,
+        decimal_day: time::decimal_day(&day_of_month),
+        cal_type: time::CalType::Gregorian,
+    };
+    let julian_day = time::julian_day(&date);
+
+    let delta_t = time::delta_t(date.year as i32, date.month);
+    let julian_ephem_day = time::julian_ephemeris_day(julian_day, delta_t);
+
+    // 2 animation
     let mut window = Window::new("Kiss3d: rectangle");
     let mut rect = window.add_rectangle(50.0, 150.0);
     let mut circ = window.add_circle(50.0);
